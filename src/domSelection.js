65615fa8d982f3bb56058c1df17b71a710437984
gliese-backup <glieseb+1@proton.me>
2024-11-13 14:37:06 +0000

    implement random logic

---
+++ src/domSelection.js
@@ -0,0 +1,6 @@
+const formEl = document.querySelector("form");
+const inputEl = document.querySelector("input");
+const yearEl = document.querySelector(".year");
+
+export { formEl, inputEl, yearEl };
+// shorthand
--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 import "./index.css";
+import { formEl, inputEl, yearEl } from "./domSelection";
 
-// MARK: DOM Selection
-const formEl = document.querySelector("form");
-const inputEl = document.querySelector("input");
-const yearEl = document.querySelector(".year");
+const tasks = [];
 
 formEl.addEventListener("submit", (e) => {
   e.preventDefault(); // Prevent the page from reloading
-  console.log(inputEl.value);
+
+  //  Checking for empty input
+  if (!inputEl.value) {
+    return;
+  }
+
+  tasks.push({
+    title: inputEl.value,
+    isCompleted: false,
+    id: crypto.randomUUID(),
+  });
+
+  console.log(tasks);
 
   //  Empty the input field
   inputEl.value = "";
