a1f869c304a20021ed489091eb02586f48425c76
gliese-backup <glieseb+1@proton.me>
2024-11-13 13:50:17 +0000

    listening for submit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
 import "./index.css";
+
+// MARK: DOM Selection
+const formEl = document.querySelector("form");
+const inputEl = document.querySelector("input");
+const yearEl = document.querySelector(".year");
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault(); // Prevent the page from reloading
+  console.log(inputEl.value);
+
+  //  Empty the input field
+  inputEl.value = "";
+});
+
+// IIFE
+(function () {
+  const year = new Date().getFullYear();
+
+  // MARK: Update the DOM
+  yearEl.textContent = `${year}`;
+})();
